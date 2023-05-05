dffe3b7dda77fc11545bc14b6f880103648a7f84
AlexFlores6651 <106271632+AlexFlores6651@users.noreply.github.com>
2023-05-05 01:01:13 -0600

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, VERSION } from '@angular/core';
+import { CapitalizadoPipe } from './capitalizado.pipe';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
- 
+export class AppComponent {
+  nombre = 'mauel aLEJANDRO flores ramirez';
 }
